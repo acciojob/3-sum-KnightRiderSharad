c254fc2ae68b17675e6f7ada6b22296e95da8f29
Kumar Sharad <114303424+KnightRiderSharad@users.noreply.github.com>
2023-01-21 20:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,9 +22,4 @@ function threeSum(arr, target) {
     // Return the closest sum found
     return closestSum;
 }
-let arr = [ -1, 2, 1, -4 ];
-let target = 1;
- 
-document.write(solution(arr, target));
-}
 module.exports = threeSum;
